115938b310ff1da165e7e9964f04e7ba04bf2bff
Hollar14 <i_lawal@outlook.com>
2024-02-21 12:11:51 +0100

    My Portfolio

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    id: 1,
+    img: "https://i.postimg.cc/zDw8Qdpr/work.png",
+    // img: "https://i.postimg.cc/VkRWdHWx/57214s81.jpg",
+    title: "Multimedia Web",
+    desc: "Consultant Agency for your Business plan and strategy ",
+    link: "https://hollar14.github.io/innkeeper_assignment_project/",
+    git: "https://github.com/Hollar14/innkeeper_assignment_project",
+  },
+  {
+    id: 2,
+    img: "https://i.postimg.cc/Bv75CLyN/4167022.jpg",
+    title: "Tarvel Agency",
+    link: "",
+    git: "https://github.com/Hollar14",
+  },
+  {
+    id: 3,
+    // img: "https://i.postimg.cc/wBjcJcW7/5890625.jpg",
+    img: "https://i.postimg.cc/DyvDfFkx/payment-site.png",
+    title: "Payment Platform",
+    desc: "",
+    link: "https://hollar14.github.io/payment_platform/",
+    git: "https://github.com/Hollar14/payment_platform",
+  },
+];
